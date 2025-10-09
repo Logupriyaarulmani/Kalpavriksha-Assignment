46c817339ff3a4aead6b65e3d182586d063e2c29
Logupriyaarulmani <logupriya535@gmail.com>
2025-10-09 14:06:43 +0530

    Updated Calculator code

--- Calculator.c
+++ Calculator.c
@@ -1,122 +1,162 @@
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
-#define MAX 100
-int intStack[MAX];
-int intTop=-1;
-void pushInt(int val){
-    intStack[++intTop]=val;
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h> 
+const int MAX = 100;
+int intStack[100];
+int intTop = -1;
+void pushInt(int value) {
+    intStack[++intTop] = value;
 }
-int popInt(){
+int popInt() {
     return intStack[intTop--];
 }
-int isEmptyInt(){
-    return intTop==-1;
+bool isEmptyInt() {
+    return intTop == -1;
 }
-char charStack[MAX];
-int charTop = -1;
-void pushChar(char c){
-    charStack[++charTop]=c;
+char operatorStack[100];
+int operatorTop = -1;
+void pushOperator(char operator) {
+    operatorStack[++operatorTop] = operator;
 }
-char popChar(){
-    return charStack[charTop--];
+char popOperator() {
+    return operatorStack[operatorTop--];
 }
-char peekChar(){
-    return charStack[charTop];
+char peekOperator() {
+    return operatorStack[operatorTop];
 }
-int isEmptyChar(){
-    return charTop==-1;
+bool isEmptyOperator() {
+    return operatorTop == -1;
 }
-int precedence(char op){
-    if (op=='+'||op=='-') return 1;
-    if (op=='*'||op=='/') return 2;
-    return 0;
+int precedence(char operator) {
+    switch (operator) {
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
+        default:
+            return 0;
+    }
 }
-int isOperator(char c){
-    return (c=='+'|| c=='-'||c=='*'||c=='/');
+bool isOperator(char symbol) {
+    return (symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/');
 }
-int infixToPostfix(char *infix,char postfix[][MAX]){
-    int i=0,k=0;
-    while(infix[i]!='\0'){
-        if(isspace(infix[i])){ 
-            i++; 
-            continue; 
-        }
-        if(isdigit(infix[i])){
-            char num[MAX];
-            int j=0;
-            while(isdigit(infix[i])){
-                num[j++] = infix[i++];
-            }
-            num[j]='\0';
-            strcpy(postfix[k++],num);
-        }
-        else if(isOperator(infix[i])){
-            while(!isEmptyChar()&&precedence(peekChar())>=precedence(infix[i])){
-                char op[2];
-                op[0]=popChar();
-                op[1]='\0';
-                strcpy(postfix[k++],op);
-            }
-            pushChar(infix[i]);
+int infixToPostfix(char *infix, char postfix[][100]) {
+    int i = 0, k = 0;
+    while (infix[i] != '\0') {
+        if (isspace(infix[i])) {
             i++;
+            continue;
         }
-        else{
-            return -1;
+        if (isdigit(infix[i])) {
+            char number[100];
+            int j = 0;
+            while (isdigit(infix[i])) {
+                number[j++] = infix[i++];
+            }
+            number[j] = '\0';
+            strcpy(postfix[k++], number);
+        } 
+        else if (isOperator(infix[i])) {
+            while (!isEmptyOperator() &&
+                   precedence(peekOperator()) >= precedence(infix[i])) {
+                char op[2] = { popOperator(), '\0' };
+                strcpy(postfix[k++], op);
+            }
+            pushOperator(infix[i]);
+            i++;
+        } 
+        else {
+            return -1; 
         }
     }
-    while(!isEmptyChar()){
-        char op[2];
-        op[0]=popChar();
-        op[1]='\0';
-        strcpy(postfix[k++],op);
+    while (!isEmptyOperator()) {
+        char op[2] = { popOperator(), '\0' };
+        strcpy(postfix[k++], op);
     }
     return k;
 }
-int finalResult;
-int evalPostfix(char postfix[][MAX], int len){
+int evalPostfix(char postfix[][100], int len, int *errorFlag) {
     intTop = -1;
-    for (int i=0;i<len;i++){
-        if (isdigit(postfix[i][0])){
-            int val=0;
-            for (int j=0;postfix[i][j]!='\0';j++){
-                val=val*10+(postfix[i][j]-'0');
+    for (int i = 0; i < len; i++) {
+        if (isdigit(postfix[i][0])) {
+            int value = 0;
+            for (int j = 0; postfix[i][j] != '\0'; j++) {
+                value = value * 10 + (postfix[i][j] - '0');
             }
-            pushInt(val);
-        }
+            pushInt(value);
+        } 
         else {
-            if (intTop < 1) return 2;
-            int b=popInt();
-            int a=popInt();
-            if (postfix[i][0]=='+') pushInt(a+b);
-            else if (postfix[i][0]=='-') pushInt(a-b);
-            else if (postfix[i][0]=='*') pushInt(a*b);
-            else if (postfix[i][0]=='/'){
-                if (b==0) return 1;
-                pushInt(a/b);
+            if (intTop < 1) {
+                *errorFlag = 2; 
+                return 0;
+            }
+            int b = popInt();
+            int a = popInt();
+            switch (postfix[i][0]) {
+                case '+':
+                    pushInt(a + b);
+                    break;
+                case '-':
+                    pushInt(a - b);
+                    break;
+                case '*':
+                    pushInt(a * b);
+                    break;
+                case '/':
+                    if (b == 0) {
+                        *errorFlag = 1;
+                        return 0;
+                    }
+                    pushInt(a / b);
+                    break;
+                default:
+                    *errorFlag = 2;
+                    return 0;
             }
         }
     }
-    finalResult=popInt();
-    return 0;
+
+    return popInt();
 }
-int main(){
-    char infix[MAX];
-    if (!fgets(infix, MAX, stdin)){
-        printf("Error:Invalid expression.\n");
-        return 0;
+int main() {
+    char infix[100];
+    char postfix[100][100];
+    int length, errorFlag;
+    while (true) {
+        printf("\nEnter an infix expression (or type 'exit' to quit): ");
+        if (!fgets(infix, MAX, stdin)) {
+            printf("Error: Invalid input.\n");
+            continue;
+        }
+        int len = strlen(infix);
+        if (infix[len - 1] == '\n') {
+            infix[len - 1] = '\0';
+        }
+        if (strcmp(infix, "exit") == 0) {
+            printf("Exiting program...\n");
+            break;
+        }
+        length = infixToPostfix(infix, postfix);
+        if (length == -1) {
+            printf("Error: Invalid expression.\n");
+            continue;
+        }
+        errorFlag = 0;
+        int result = evalPostfix(postfix, length, &errorFlag);
+        switch (errorFlag) {
+            case 1:
+                printf("Error: Division by zero.\n");
+                break;
+            case 2:
+                printf("Error: Invalid expression.\n");
+                break;
+            default:
+                printf("Result: %d\n", result);
+                break;
+        }
     }
-    int len=strlen(infix);
-    if (infix[len-1] == '\n') infix[len-1]='\0';
-    char postfix[MAX][MAX];
-    len=infixToPostfix(infix,postfix);
-    if (len==-1){
-        printf("Error:Invalid expression.\n");
-        return 0;
-    }
-    int error=evalPostfix(postfix,len);
-    if (error==1) printf("Error:Division by zero.\n");
-    else if (error==2) printf("Error:Invalid expression.\n");
-    else printf("%d\n",finalResult);
     return 0;
 }
