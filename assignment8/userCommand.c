cee02e02a7bc883c831a97fe5637e39f05ccb041
Logupriyaarulmani <logupriya535@gmail.com>
2025-11-24 19:22:01 +0530

    Completed Assignment8 LRU Cache

--- assignment8/userCommand.c
+++ assignment8/userCommand.c
@@ -2,32 +2,78 @@
 #include "LRUCache.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 void runUserCommandLoop() {
-    char command[20];
+    char command[MAX_VALUE_LENGTH];
     LRUCache* cache = NULL;
 
-    while (1) {
-        scanf("%s", command);
+    bool isRunning = true;
 
-        if (strcmp(command, "createCache") == 0) {
-            int cap; scanf("%d", &cap);
-            cache = lruCreate(cap);
+    while (isRunning) {
+        fgets(command, sizeof(command), stdin);
+        
+        if (fgets(command, sizeof(command), stdin) == NULL) {
+            break; 
         }
-        else if (strcmp(command, "put") == 0) {
-            int key; char value[100];
-            scanf("%d %s", &key, value);
-            lruPut(cache, key, value);
-        }
-        else if (strcmp(command, "get") == 0) {
-            int key; scanf("%d", &key);
-            lruGet(cache, key);
-        }
-        else if (strcmp(command, "exit") == 0) {
-            break;
-        }
-        else {
-            printf("Enter Valid Command!\n");
+
+        switch (command[0]){
+            case 'c':
+                if (strcmp(command, "createCache") == 0)
+                {
+                    int newCapacity;
+                    scanf("%d", &newCapacity);
+                    createCacheTable(newCapacity);
+                }
+                else
+                {
+                    printf("Enter Valid Command!\n");
+                }
+                break;
+            
+            case 'p':
+                if (strcmp(command, "put") == 0)
+                {
+                    int newKey;
+                    char value[100];
+                    scanf("%d", &newKey);
+                    scanf("%s", value);
+                    put(newKey, value);
+                }
+                else
+                {
+                    printf("Enter Valid Command!\n");
+                }
+                break;
+
+            case 'g':
+                if (strcmp(command, "get") == 0)
+                {
+                    int currKey;
+                    scanf("%d", &currKey);
+                    get(currKey);
+                }
+                else
+                {
+                    printf("Enter Valid Command!\n");
+                }
+                break;
+
+            case 'e':
+                if (strcmp(command, "exit") == 0)
+                {
+                    isRunning = 0;
+                    break;
+                }
+                else
+                {
+                    printf("Enter Valid Command!\n");
+                }
+                break;
+
+            default:
+                printf("Enter Valid Command!\n");
+                break;
         }
     }
 }
