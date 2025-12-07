1e26bab46dedf01e1eda4324d17f852e8a4b8a25
Logupriyaarulmani <logupriya535@gmail.com>
2025-12-07 19:04:41 +0530

    Moved menu() function to icc_main.c

--- assignment7/icc_main.c
+++ assignment7/icc_main.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#include "menu.h"
 #include "team.h"
 #include "player.h"
 #include "globals.h"
+
+void menu ()
+{
+    printf("\n==============================================================================");
+    printf("\nICC ODI Player Performance Analyzer");
+    printf("\n==============================================================================");
+    printf("\n1. Add Player to Team");
+    printf("\n2. Display Players of a Specific Team");
+    printf("\n3. Display Teams by Average Batting Strike Rate");
+    printf("\n4. Display Top K Players of a Specific Team by Role");
+    printf("\n5. Display all Players of Specific Role Across All Teams by Performance Index");
+    printf("\n6. Exit");
+    printf("\n==============================================================================");
+}
 
 int main ()
 {
